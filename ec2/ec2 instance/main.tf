281b7fb9d49e65cfa036576178d6ac4f52a41562
Aswanidevm <maswanidev@gmail.com>
2023-08-01 12:37:59 +0530

    securitygp

--- ec2/ec2 instance/main.tf
+++ ec2/ec2 instance/main.tf
@@ -7,9 +7,35 @@ data "aws_ami" "ec2" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ec2.id
   instance_type = var.instance_type
-
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = var.component
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
 
