ecd9b30706b0cb8b5044642186f67be345296111
Aswanidevm <maswani.dev@gmail.com>
2024-05-29 18:57:39 +0530

    add

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source ="./ec2 instance"
+    source ="ec2 instance"
     for_each = var.instances
     component =  each.value["name"]
     instance_type = lookup(each.value, "instance_type", "t2.micro" )
